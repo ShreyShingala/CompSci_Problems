86836da3a93aaac71674a35132fa7b77f42e3166
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-10-05 17:14:43 -0400

    HASHMAPPPPP

---
+++ Data_Implementation/Hashmap.py
@@ -0,0 +1,84 @@
+#index = key%available adresses
+
+#Objectives, Minimize collisions, uniform distribution, easy to calculate, resolve solutions
+
+from Linked_list import *
+
+def AddPrefectData():
+    Hashmap.AddData("Mia")
+    Hashmap.AddData("Tim")
+    Hashmap.AddData("Bae")
+    Hashmap.AddData("Zoe")
+    Hashmap.AddData("Jan")
+    Hashmap.AddData("Ada")
+    Hashmap.AddData("Leo")
+    Hashmap.AddData("Sam")
+    Hashmap.AddData("Lou")
+    Hashmap.AddData("Max")
+    Hashmap.AddData("Ted")
+
+def AddImperfectData():
+    Hashmap.AddData("Mia")
+    Hashmap.AddData("Tim")
+    Hashmap.AddData("Bae")
+    Hashmap.AddData("Zoe")
+    Hashmap.AddData("Sue")
+    Hashmap.AddData("Len")
+    Hashmap.AddData("Moe")
+    Hashmap.AddData("Lou")
+    Hashmap.AddData("Rae")
+    Hashmap.AddData("Max")
+    Hashmap.AddData("Tod")
+    
+
+class Hashmap():
+    def __init__(self, size):
+        self.size = size
+        self.Hashtable = [None] * size # * round(size * 1.5) KJHKJHKJHKJHKJHKJKJ
+        
+    def PrintTable(self):
+        currstring = "["
+        for i in range(len(self.Hashtable)):
+            if self.Hashtable[i] == None:
+                currstring += "None, "
+            else:
+                linkylist = LinkedList()
+                currstring += linkylist.ReturnPrintString(self.Hashtable[i])
+                
+        print(currstring)
+        
+    def HashFunc(self, Data):
+        key = 0
+        for chr in Data:
+            key += ord(chr)
+
+        key = key % self.size
+        return key
+                
+    def AddData(self, Data):
+        key = self.HashFunc(Data)
+        if self.Hashtable[key] == None:
+            linkylist = LinkedList()
+            self.Hashtable[key] = linkylist.MakeNode(Data, None)
+        else:
+            linkylist = LinkedList()
+            linkylist.AddFinalElem(Data, self.Hashtable[key])
+
+    def FindData(self, Data):
+        key = self.HashFunc(Data)
+        linkylist = LinkedList()
+        temp = self.Hashtable[key]
+        if Data == linkylist.temp:
+            print("Found " + str(Data) + " at index " + str(key))
+        else:
+            linkylist = LinkedList()
+            linkylist.Find(Data, self.Hashtable[key])
+
+Hashmap = Hashmap(11)
+
+#AddPrefectData()
+AddImperfectData()
+
+Hashmap.PrintTable()
+
+Hashmap.FindData("Tim")
--- Data_Implementation/Linked_list.py
+++ Data_Implementation/Linked_list.py
@@ -1,162 +1,186 @@
-# we could turn the entire list itself into a class?
 
 class Node():
     def __init__(self, datavalue=None, nextelem=None):
         self.datavalue = datavalue
         self.nextelem = nextelem
-
-def PrintList(head):
-    temp = head
-    while temp:
-        print(temp.datavalue)
-        temp = temp.nextelem
-
-def Find(val, head):
-    temp = head
-    i = -1
-    while temp:
-        i += 1
-        data = temp.datavalue
-        if str(data) == str(val):
-            print("Found " + str(data) + " at index " + str(i))
-            return None
-        temp = temp.nextelem
     
-    print(str(data) + " is not in the Linked List")
+class LinkedList():
+    def __init__(self):
+        pass
 
-def AddFinalElem(val, head):
-    temp = head
-    while temp.nextelem != None:
-        temp = temp.nextelem
-    newlastelem = Node(val, None)
-    temp.nextelem = newlastelem
-    return newlastelem
+    def MakeNode(self, datavalue, nextelem):
+        return Node(datavalue, nextelem)
 
-def AddFirstElem(val, head):
-    temp = head
-    head = Node(val, temp)
-    return head
+    def ReturnPrintString(self, head):
+        temp = head
+        curstring = "["
+        
+        if head.nextelem == None:
+            return temp.datavalue + ", "
+        
+        while temp:            
 
-def RemoveFirstElem(head):
-    temp = head.nextelem
-    return temp
+            if temp.nextelem != None: 
+                curstring += str(temp.datavalue) + ", "
+            else:
+                curstring += str(temp.datavalue)
+            temp = temp.nextelem
+        curstring += "], "
+        return curstring
+        
 
-def RemoveFinalElem(head):
-    temp = head
-    temp1 = None
-    while temp.nextelem != None:
-        temp1 = temp
-        temp = temp.nextelem
-    temp1.nextelem = None
-    return temp1
+    def PrintList(self, head):
+        temp = head
+        while temp:
+            print(temp.datavalue)
+            temp = temp.nextelem
 
-def RemoveAtIndex(index, head):
-    temp = head
-    i = 0
-    
-    while i < index + 2:
-        if i == (index-1):
-            firstvalue = temp
-        if i == (index+1):
-            secondvalue = temp
-        temp = temp.nextelem
+    def Find(self, val, head):
+        temp = head
+        i = -1
+        while temp:
+            i += 1
+            data = temp.datavalue
+            if str(data) == str(val):
+                print("Found " + str(data) + " at index " + str(i))
+                return None
+            temp = temp.nextelem
+        
+        print(str(data) + " is not in the Linked List")
+
+    def AddFinalElem(self, val, head):
+        temp = head
+        while temp.nextelem != None:
+            temp = temp.nextelem
+        newlastelem = Node(val, None)
+        temp.nextelem = newlastelem
+        return newlastelem
+
+    def AddFirstElem(self, val, head):
+        temp = head
+        head = Node(val, temp)
+        return head
+
+    def RemoveFirstElem(self, head):
+        temp = head.nextelem
+        return temp
+
+    def RemoveFinalElem(self, head):
+        temp = head
+        temp1 = None
+        while temp.nextelem != None:
+            temp1 = temp
+            temp = temp.nextelem
+        temp1.nextelem = None
+        return temp1
+
+    def RemoveAtIndex(self, index, head):
+        temp = head
+        i = 0
+        
+        while i < index + 2:
+            if i == (index-1):
+                firstvalue = temp
+            if i == (index+1):
+                secondvalue = temp
+            temp = temp.nextelem
+                
+            i += 1
+        
+        firstvalue.nextelem = secondvalue
             
-        i += 1
-    
-    firstvalue.nextelem = secondvalue
+    def ChangeAtIndex(self, newval, index, head):
+        temp = head
+        i = 0
         
-def ChangeAtIndex(newval, index, head):
-    temp = head
-    i = 0
-    
-    while i < index + 1:
-        if i == (index):
-            temp.datavalue = newval
-        temp = temp.nextelem
+        while i < index + 1:
+            if i == (index):
+                temp.datavalue = newval
+            temp = temp.nextelem
+                
+            i += 1
+
+    def AppendAtIndex(self, val, index, head):
+        temp = head
+        value = Node(val, None)
+        
+        i = 0
+        
+        while i < index:
             
-        i += 1
+            if i == (index - 1):
+                nextnode = temp.nextelem
+                temp.nextelem = value
+                value.nextelem = nextnode
+                break
+            
+            temp = temp.nextelem
+            
+            i += 1
+            
+        return value
 
-def AppendAtIndex(val, index, head):
-    temp = head
-    value = Node(val, None)
-    
-    i = 0
-    
-    while i < index:
+    def GetLength(self, head):
+        length = 0
+        temp = head
         
-        if i == (index - 1):
-            nextnode = temp.nextelem
-            temp.nextelem = value
-            value.nextelem = nextnode
-            break
+        while temp:
+            
+            length += 1
+            temp = temp.nextelem
+            
+        print("List is " + str(length) + " items long")
+        return length
+
+    def Reverse(self, head):
+        temp = head
+        previous = head
+        nextelem = head.nextelem
+        head.nextelem = None
         
-        temp = temp.nextelem
+        while temp:
+            
+            temp = nextelem
+            nextelem = temp.nextelem 
+            temp.nextelem = previous
+            previous = temp
+            
+            if nextelem == None:
+                break
         
-        i += 1
-        
-    return value
+        return temp        
 
-def GetLength(head):
-    length = 0
-    temp = head
-    
-    while temp:
-        
-        length += 1
-        temp = temp.nextelem
-        
-    print("List is " + str(length) + " items long")
-    return length
+#linkylist = LinkedList()
 
-def Reverse(head):
-    temp = head
-    previous = head
-    nextelem = head.nextelem
-    head.nextelem = None
-    
-    while temp:
-        
-        temp = nextelem
-        nextelem = temp.nextelem 
-        temp.nextelem = previous
-        previous = temp
-        
-        if nextelem == None:
-            break
+#elem5 = linkylist.MakeNode("5", None)    
+#elem4 = linkylist.MakeNode("4", elem5)
+#elem3 = linkylist.MakeNode("3", elem4)
+#elem2 = linkylist.MakeNode("2", elem3)
+#elem1 = linkylist.MakeNode("1", elem2)
 
-        
-        
-    return temp        
+#head = elem1
 
-elem5 = Node("5", None)    
-elem4 = Node("4", elem5)
-elem3 = Node("3", elem4)
-elem2 = Node("2", elem3)
-elem1 = Node("1", elem2)
+#head = linkylist.AddFirstElem("FIRST", head)
+#head = linkylist.AddFirstElem("BETTERFIRST", head)
 
-head = elem1
+#tail = linkylist.AddFinalElem("LAST", head)
+#tail = linkylist.AddFinalElem("better Last", head)
 
-head = AddFirstElem("FIRST", head)
-head = AddFirstElem("BETTERFIRST", head)
+#linkylist.RemoveAtIndex(4, head)
+#linkylist.ChangeAtIndex("Guacamole", 4, head)
 
-tail = AddFinalElem("LAST", head)
-tail = AddFinalElem("better Last", head)
+#head = linkylist.RemoveFirstElem(head)
 
-RemoveAtIndex(4, head)
-ChangeAtIndex("Guacamole", 4, head)
+#tail = linkylist.RemoveFinalElem(head)
 
-head = RemoveFirstElem(head)
+#linkylist.PrintList(head)
 
-tail = RemoveFinalElem(head)
+#linkylist.AppendAtIndex("Appended", 2, head)
 
-PrintList(head)
+#linkylist.PrintList(head)
 
-AppendAtIndex("Appended", 2, head)
+#linkylist.GetLength(head)
 
-PrintList(head)
+#head = linkylist.Reverse(head)
 
-GetLength(head)
-
-head = Reverse(head)
-
-PrintList(head)
+#linkylist.PrintList(head)
