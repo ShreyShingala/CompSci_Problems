ba017a21d4fcc94f5ec9923e9bf1995f219436fc
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-10-12 14:50:05 -0400

    DOUBLE_LINKED

--- Data_Implementation/Linked_list_double.py
+++ Data_Implementation/Linked_list_double.py
@@ -1,14 +1,14 @@
-#8. Write a Python program to create a doubly linked list, append some items and iterate through the list (print forward).
-#9. Write a Python program to create a doubly linked list and print nodes from current position to first node.
-#10. Write a Python program to count the number of items of a given doubly linked list.
-#11. Write a Python program to print a given doubly linked list in reverse order.
-#12. Write a Python program to insert an item in front of a given doubly linked list.
-#13. Write a Python program to search a specific item in a given doubly linked list and return true if the item is found otherwise return false.
+#DONE -  Write a Python program to create a doubly linked list, append some items and iterate through the list (print forward).
+#DONE Write a Python program to create a doubly linked list and print nodes from current position to first node.
+#DONE Write a Python program to count the number of items of a given doubly linked list.
+#DONE Write a Python program to print a given doubly linked list in reverse order.
+#DONE Write a Python program to insert an item in front of a given doubly linked list.
+#DONE Write a Python program to search a specific item in a given doubly linked list and return true if the item is found otherwise return false.
 #14. Write a Python program to delete a specific item from a given doubly linked list.
 
 
 class Node():
-    def __init__(self, datavalue=None, nextelem=None, prevelem = None):
+    def __init__(self, datavalue=None, nextelem=None, prevelem=None):
         self.datavalue = datavalue
         self.nextelem = nextelem
         self.prevelem = prevelem
@@ -38,12 +38,27 @@ class LinkedList():
         curstring += "], "
         return curstring
         
+    def PrintFrom(self, index, head):
+        temp = head
+        i = -1
+        while temp:
+            i += 1
+            if i >= index:
+                print(temp.datavalue)
+                
+            temp = temp.nextelem
 
     def PrintList(self, head):
         temp = head
         while temp:
             print(temp.datavalue)
             temp = temp.nextelem
+            
+    def PrintListReverse(self, tail):
+        temp = tail
+        while temp:
+            print(temp.datavalue)
+            temp = temp.prevelem
 
     def Find(self, val, head):
         temp = head
@@ -92,6 +107,21 @@ class LinkedList():
         newtail.nextelem = 0
         return newtail
 
+    def RemoveCertainVal(self, val, head):
+        temp = head
+        val = str(val)
+        
+        while temp:
+            
+            if val == temp.datavalue:
+                firstvalue = temp.prevelem
+                secondvalue = temp.nextelem
+            
+            temp = temp.nextelem
+        
+        firstvalue.nextelem = secondvalue
+        secondvalue.prevelem = firstvalue
+
     def RemoveAtIndex(self, index, head):
         temp = head
         i = 0
@@ -107,6 +137,7 @@ class LinkedList():
         
         firstvalue.nextelem = secondvalue
         secondvalue.prevelem = firstvalue
+        
             
     def ChangeAtIndex(self, newval, index, head):
         temp = head
@@ -128,9 +159,12 @@ class LinkedList():
         while i < index:
             
             if i == (index - 1):
+                prevelem = temp.prevelem
                 nextnode = temp.nextelem
                 temp.nextelem = value
                 value.nextelem = nextnode
+                value.prevelem = prevelem
+                nextnode.prevelem = value
                 break
             
             temp = temp.nextelem
@@ -154,13 +188,30 @@ class LinkedList():
 
 linkylist = LinkedList()
 
-elem5 = linkylist.MakeNode("5", None)    
-elem4 = linkylist.MakeNode("4", elem5)
-elem3 = linkylist.MakeNode("3", elem4)
-elem2 = linkylist.MakeNode("2", elem3)
-elem1 = linkylist.MakeNode("1", elem2)
 
-LinkedList.PrintList(elem1)
+head = linkylist.MakeNode("1", None, None)
+tail = head
+tail = linkylist.AddFinalElem("2", tail)
+tail = linkylist.AddFinalElem("3", tail)
+tail = linkylist.AddFinalElem("4", tail)
+tail = linkylist.AddFinalElem("5", tail)
+
+linkylist.PrintList(head)
+print("____________________")
+linkylist.PrintFrom(2, head)
+print("____________________")
+linkylist.GetLength(head)
+print("____________________")
+linkylist.PrintListReverse(tail)
+print("____________________")
+head = linkylist.AddFirstElem("FIRST", head)
+linkylist.PrintList(head)
+print("____________________")
+linkylist.Find(3, head)
+print("____________________")
+linkylist.RemoveAtIndex(3, head)
+linkylist.RemoveCertainVal(2, head)
+linkylist.PrintList(head)
 
 #head = elem1
 
