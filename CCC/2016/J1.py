37d3646a24a994cba3a1679fc1f464c17b00edc4
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-10-23 19:37:00 -0400

    J1

---
+++ CCC/2016/J1.py
@@ -0,0 +1,15 @@
+win = 0
+
+for i in range(6):
+    let = str(input())
+    if let == "W":
+        win +=1
+    
+if win == 0:
+    print("-1")
+elif win <= 2:
+    print("3")
+elif win <= 4:
+    print("2")
+else:
+    print("1")
