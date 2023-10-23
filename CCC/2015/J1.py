948cb28eb5570b9aeac0a20d3d57c30d217d9489
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-10-23 19:32:57 -0400

    mb_real_j1

--- CCC/2015/J1.py
+++ CCC/2015/J1.py
@@ -5,10 +5,10 @@ day = int(input())
 
 if month > 2:
     print("After")
-elif day > 18:
-    print("After")
 elif month < 2:
     print("Before")
+elif day > 18:
+    print("After")
 elif day < 18:
     print("Before")
 else:
