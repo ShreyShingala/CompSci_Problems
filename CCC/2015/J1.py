270f0c47f553cdb572bcc51e87be57ccfe72c58e
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-10-23 19:31:10 -0400

    J1

---
+++ CCC/2015/J1.py
@@ -0,0 +1,15 @@
+#Special Day
+
+month = int(input())
+day = int(input())
+
+if month > 2:
+    print("After")
+elif day > 18:
+    print("After")
+elif month < 2:
+    print("Before")
+elif day < 18:
+    print("Before")
+else:
+    print("Special")
