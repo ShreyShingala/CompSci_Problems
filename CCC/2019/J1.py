89aa0b8037695076917399dd1b7975f319976262
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-10-26 09:00:50 -0400

    J1

---
+++ CCC/2019/J1.py
@@ -0,0 +1,19 @@
+# Winning Score
+
+A3 = int(input())
+A2 = int(input())
+A1 = int(input())
+B3 = int(input())
+B2 = int(input())
+B1 = int(input())
+
+A = A3 * 3 + A2 * 2 + A1
+B = B3 * 3 + B2 * 2 + B1
+
+if A > B:
+    print("A")
+elif A < B:
+    print("B")
+else:
+    print("T")
+    
