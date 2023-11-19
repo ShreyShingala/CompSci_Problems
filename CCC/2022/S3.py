e4ef83e4d07569a503678bffa43ed72f0b20d5ad
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-11-19 12:40:15 -0500

    S3

--- CCC/2022/S3.py
+++ CCC/2022/S3.py
@@ -7,12 +7,13 @@ highest = int(specs[1])
 goodsamples = int(specs[2])
 
 done = None
+current_unique = 1
 currentset = [1] * num
 currentgoods = num
+minus_factor = 0
 
 def impossible():
     print(-1)
-    
 def possible():
     global done
     print(" ".join(map(str, currentset)))
@@ -44,10 +45,38 @@ if highest == 2:
         if currentgoods == goodsamples:
             break
 else:
-    #todo
-    pass
+    for i in range(1, num):
+        
+        current_unique += 1
+        temp = ((current_unique-minus_factor)*(current_unique - minus_factor + 1))//2 - current_unique + minus_factor
+        
+        if goodsamples == currentgoods:
+            break 
+        if (goodsamples-1) == currentgoods:
+            currentset[num-1] = num
+            currentgoods += 1
+            break
+        elif currentgoods + temp < goodsamples:
+            currentset[i] = current_unique
+        elif currentgoods + temp > goodsamples:
+            print("MORE")
+            
+            currentgoods += ((current_unique-minus_factor-1)*(current_unique - minus_factor))//2 - current_unique + minus_factor
+            currentset[i] = current_unique -1
+            minus_factor = current_unique -1
+            temp = 0
+            
+        print("CURR SET")
+        print(currentset)
+        print("Current good samples" + str(currentgoods))
+        print("The temporary goods" + str(temp))
+        
+print(currentset)      
+            
 
 if currentgoods == goodsamples:
     possible()    
 else:
     impossible()
+    
+    
