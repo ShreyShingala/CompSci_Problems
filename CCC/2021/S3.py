8994c4bbf917c5cc31af4cd225fcf4490092e023
BigBirdTheGreat <shreyshingala1@gmail.com>
2023-11-27 17:33:29 -0500

    MORE_EFFICENT

--- CCC/2021/S3.py
+++ CCC/2021/S3.py
@@ -11,26 +11,50 @@ people.sort(key=lambda x: int(x[0]))
 
 lowest = people[0][0]
 greatest = people[-1][0]
+mid = lowest+greatest//2
 
-for i in range(lowest, greatest+1):
+def curr_dist(point):
+    global people
+    
     currcheck = 0
     for p in people:
         position = p[0]
         speed = p[1]
         hearing = p[2]
-        if i > position:
-            if (position + hearing) > i:
-                continue
-            else:
-                currcheck += ((i - (position + hearing)) * speed)
-        elif i < position:
-            if (position - hearing) < i:
-                continue
-            else:
-                currcheck += (((position - hearing)- i) * speed)
-            
         
-    bestdistance = min(currcheck, bestdistance)
+        
+        if point > position:
+            if (position + hearing) > point:
+                continue
+            else:
+                currcheck += ((point - (position + hearing)) * speed)
+        elif point < position:
+            if (position - hearing) < point:
+                continue
+            else:
+                currcheck += (((position - hearing)- point) * speed)
+        if currcheck > bestdistance:
+            break
+        
+    return currcheck
+
+while True:
+    left = curr_dist(mid-1)
+    currcheck = curr_dist(mid)
+    right = curr_dist(mid+1)
+    
+    if currcheck < left and currcheck < right:
+        bestdistance = currcheck
+        break
+    elif currcheck == left or currcheck == right:
+        bestdistance = currcheck
+        break
+    elif currcheck <= right:
+        mid = mid-1
+    else:
+        mid = mid +1
+   
+
             
 
 print(bestdistance)
